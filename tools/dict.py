8ddaa720f31428318aa89ddf9004012f23ccb89d
sir <hammercui@163.com>
2022-10-14 18:05:30 +0800

    feat: 词典

--- tools/dict.py
+++ tools/dict.py
@@ -1,4 +1,15 @@
 
+words_file = '200dic.txt'
 
-sheet='20000_words-2'
+# step1 
+# 本机词典库根目录
+dic_file_root = "/home/hammer/Documents/词典词库"
 
+# mdx字典文本，mdd音频文件
+def get_dic_list(dic_name):
+    #遍历文件
+
+
+
+if __name__ == '__main__':
+    get_dic_list('/The Little Dict big')
