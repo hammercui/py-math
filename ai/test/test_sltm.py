0a9be0f1e06e1236753cc700240c73f7cb06896b
hammer <hammercui@163.com>
2023-11-23 18:53:46 +0800

    feat: poetry toml

--- ai/test/test_sltm.py
+++ ai/test/test_sltm.py
@@ -1,5 +1,13 @@
+import unittest
+
 from ai.default_args.lstm import get_lstm_default
 
-if __name__ == '__main__':
-     args = get_lstm_default()
 
+class TestDemo(unittest.TestCase):
+
+    def test_hello_world(self):
+        print(f"hello world!")
+
+    def test_lstm_default(self):
+        args = get_lstm_default()
+        print(args)
